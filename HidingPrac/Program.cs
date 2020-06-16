89788635a3b4494d45778b892e7e1eec85e2d716
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-06-16 14:42:43 +0900

    Hiding 실습

--- HidingPrac/Program.cs
+++ HidingPrac/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace HidingPrac
 {
+    class Parent
+    {
+        public int variable = 273;
+        public void Method()
+        {
+            Console.WriteLine("부모의 메서드");
+        }
+    }
+    class Child : Parent{
+        public string variable = "shadowing";
+        public void Method()
+        {
+            Console.WriteLine("자식의 메서드");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Child c = new Child();
+            Console.WriteLine(c.variable);
+            Console.WriteLine(((Parent)c).variable);
+
+            c.Method();
+            ((Parent)c).Method();
         }
     }
 }
